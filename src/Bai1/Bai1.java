201106edbf5df7f0f79d094fcee2e10e0a504d42
Bui Van Dung <buivandung279@gmail.com>
2022-02-06 20:08:27 +0700

    first commit

---
+++ src/Bai1/Bai1.java
@@ -0,0 +1,24 @@
+package Bai1;
+
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a,b;
+        System.out.println("Nhập a:");
+        a = scanner.nextInt();
+        System.out.println("Nhập b:");
+        b = scanner.nextInt();
+        int tong = a + b;
+        System.out.println("Tổng là :"+tong);
+        int hieu = a-b;
+        System.out.println("Hiệu là :"+hieu);
+        int tich = a*b;
+        System.out.println("Tích là :"+tich);
+        int thuong = a/b;
+        System.out.println("Thương là :"+thuong);
+        int mod = a%b;
+        System.out.println("Mod là :"+mod);
+    }
+}
